70fa2b765722d76210251ff7afbd6333ee654ec6
Novak <novak.simic21@gmail.com>
2020-03-19 12:36:56 +0100

    Init commit

---
+++ src/db/mongoose.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const mongodb_url = process.env.MONGODB_URL
+
+mongoose.connect(mongodb_url, {
+    useCreateIndex: true,    
+    useNewUrlParser: true,
+    useFindAndModify: false
+    
+})
+
